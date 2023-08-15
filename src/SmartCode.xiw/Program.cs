b62b5ca07779034cb55e190dd4e91ed7cfc1cfed
zhongxiwang <Zxw@202307>
2023-08-15 15:11:04 +0800

    init

---
+++ src/SmartCode.xiw/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using System.Threading;
+using System.IO;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace SmartCode.xiw
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(1000, 1000);
+            ThreadPool.SetMaxThreads(1000, 1000);
+            await CommandLineApplication.ExecuteAsync<SmartCodeCommand>(args);
+        }
+    }
+}
